95e82f45ffcb5963cb3795857f3bc8eb809dade8
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2018-10-09 09:27:08 +0200

    Boletin1_3

---
+++ Boletin1_3/src/boletin1_3/Boletin1_3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_3;
+
+/**
+ *
+ * @author jmartinezpineiro
+ */
+public class Boletin1_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float euro = 100, valorDolar = 1.16f;
+        System.out.println(euro+" euros son "+euro*valorDolar+" dolares");
+        
+        // TODO code application logic here
+    }
+    
+}
