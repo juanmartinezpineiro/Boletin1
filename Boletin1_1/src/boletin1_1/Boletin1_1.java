4d3ac22a45aca925738afcb49e4bfccfbc1d9835
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2018-10-09 09:26:39 +0200

    Boletin1_1

---
+++ Boletin1_1/src/boletin1_1/Boletin1_1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_1;
+
+/**
+ *
+ * @author jmartinezpineiro
+ */
+public class Boletin1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         float base= 4.3f, altura= 3f, area;
+        area = base*altura;
+        System.out.println(area);
+    }
+    
+}
